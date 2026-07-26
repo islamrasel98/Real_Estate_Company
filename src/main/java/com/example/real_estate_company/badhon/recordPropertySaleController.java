e44d9b6c36acc4027ec05941d2fc4f6377f2aa88
Badhon <nandibadhon0@gmail.com>
2026-07-27 01:28:15 +0600

    Property Sale Fxml

---
+++ src/main/java/com/example/real_estate_company/badhon/recordPropertySaleController.java
@@ -0,0 +1,7 @@
+package com.example.real_estate_company.badhon;
+
+public class recordPropertySaleController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ src/main/java/com/example/real_estate_company/badhon/schedulePropertyVisitController.java
@@ -0,0 +1,7 @@
+package com.example.real_estate_company.badhon;
+
+public class schedulePropertyVisitController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
