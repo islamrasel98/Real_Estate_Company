4819e00298f40df56cee6084f27038e1b8c88bf1
Badhon <nandibadhon0@gmail.com>
2026-08-06 22:02:37 +0600

    Record company Expenses fxml done

--- src/main/java/com/example/real_estate_company/badhon/recordCompanyExpensesController.java
+++ src/main/java/com/example/real_estate_company/badhon/recordCompanyExpensesController.java
@@ -1,7 +1,30 @@
 package com.example.real_estate_company.badhon;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
 public class recordCompanyExpensesController
 {
     @javafx.fxml.FXML
+    private DatePicker expenseDateDatePicker;
+    @javafx.fxml.FXML
+    private TextField expenseTitleTextField;
+    @javafx.fxml.FXML
+    private ComboBox categoryComboBox;
+    @javafx.fxml.FXML
+    private TextField amountTextField;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void addExpenseOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void saveExpenseOnAction(ActionEvent actionEvent) {
+    }
+}
