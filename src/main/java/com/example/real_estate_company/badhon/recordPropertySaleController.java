647ffa681e0eae01c6e3c0677b4ee93fd3ae7a6f
Badhon <nandibadhon0@gmail.com>
2026-08-06 22:24:28 +0600

    record property fxml done

--- src/main/java/com/example/real_estate_company/badhon/recordPropertySaleController.java
+++ src/main/java/com/example/real_estate_company/badhon/recordPropertySaleController.java
@@ -1,7 +1,56 @@
 package com.example.real_estate_company.badhon;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class recordPropertySaleController
 {
     @javafx.fxml.FXML
+    private DatePicker saleDateDatePicker;
+    @javafx.fxml.FXML
+    private TableColumn amountTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn idTableColumn;
+    @javafx.fxml.FXML
+    private TableView installmentTypeTableView;
+    @javafx.fxml.FXML
+    private TextField customerSearchTextField;
+    @javafx.fxml.FXML
+    private ComboBox pymentTypeComboBox;
+    @javafx.fxml.FXML
+    private TextField bookingAmountTextField;
+    @javafx.fxml.FXML
+    private TableColumn projectTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn statusTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn balanceTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn priceTableColumn;
+    @javafx.fxml.FXML
+    private Label saleSummaryLabel;
+    @javafx.fxml.FXML
+    private TableColumn typeTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn dueDateTableColumn;
+    @javafx.fxml.FXML
+    private TableView propertyTableView;
+    @javafx.fxml.FXML
+    private TableColumn statusTavleColumn;
+    @javafx.fxml.FXML
+    private TextField agreedPriceTextField;
+    @javafx.fxml.FXML
+    private TableColumn noTableColumn;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void customerIdSearchOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void comfirmSaleOnAction(ActionEvent actionEvent) {
+    }
+}
