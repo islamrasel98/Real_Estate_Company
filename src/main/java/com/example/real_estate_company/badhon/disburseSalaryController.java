87ca9ca87ffdea27ea3fe36e88441f870e00fb71
Badhon <nandibadhon0@gmail.com>
2026-08-06 18:48:31 +0600

    Disburse Salary fxml complete

--- src/main/java/com/example/real_estate_company/badhon/disburseSalaryController.java
+++ src/main/java/com/example/real_estate_company/badhon/disburseSalaryController.java
@@ -1,7 +1,48 @@
 package com.example.real_estate_company.badhon;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class disburseSalaryController
 {
     @javafx.fxml.FXML
+    private TableColumn nameTableColom;
+    @javafx.fxml.FXML
+    private TableColumn deptTableColom;
+    @javafx.fxml.FXML
+    private TableView EmployeeTable;
+    @javafx.fxml.FXML
+    private TableColumn basicTableColom;
+    @javafx.fxml.FXML
+    private ComboBox paymenrMethodComboBox;
+    @javafx.fxml.FXML
+    private TableView salarySummaryTable;
+    @javafx.fxml.FXML
+    private TableColumn emppIdTableColom;
+    @javafx.fxml.FXML
+    private TableColumn empIdTableColom;
+    @javafx.fxml.FXML
+    private TableColumn grossTableColom;
+    @javafx.fxml.FXML
+    private TableColumn netPayTableColom;
+    @javafx.fxml.FXML
+    private TableColumn deductTableColom;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void disburseSalaryOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void generateSalarySheetOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void generateEmployeeSalaryOnAction(ActionEvent actionEvent) {
+    }
+}
