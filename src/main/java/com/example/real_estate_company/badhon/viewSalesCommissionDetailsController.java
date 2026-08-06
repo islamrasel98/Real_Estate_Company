1e77fab741737ee3cf0be63d0a1f606240ab8da5
Badhon <nandibadhon0@gmail.com>
2026-08-06 23:11:03 +0600

    Sale commission fxml done

--- src/main/java/com/example/real_estate_company/badhon/viewSalesCommissionDetailsController.java
+++ src/main/java/com/example/real_estate_company/badhon/viewSalesCommissionDetailsController.java
@@ -1,7 +1,23 @@
 package com.example.real_estate_company.badhon;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
 public class viewSalesCommissionDetailsController
 {
     @javafx.fxml.FXML
+    private ComboBox yearComboBox;
+    @javafx.fxml.FXML
+    private ComboBox monthComboBox;
+    @javafx.fxml.FXML
+    private Label salesCommissionDetailsLabel;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void viewOnAction(ActionEvent actionEvent) {
+    }
+}
