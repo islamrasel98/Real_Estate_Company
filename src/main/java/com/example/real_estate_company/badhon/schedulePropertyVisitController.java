4142b05b00fba13e55396361dbdfeb86ff36c138
Badhon <nandibadhon0@gmail.com>
2026-08-06 22:38:01 +0600

    Property visit   fxml done

--- src/main/java/com/example/real_estate_company/badhon/schedulePropertyVisitController.java
+++ src/main/java/com/example/real_estate_company/badhon/schedulePropertyVisitController.java
@@ -1,7 +1,26 @@
 package com.example.real_estate_company.badhon;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
 public class schedulePropertyVisitController
 {
     @javafx.fxml.FXML
+    private TextField serachCustomerTextField;
+    @javafx.fxml.FXML
+    private ComboBox propertyTypeComboBox;
+    @javafx.fxml.FXML
+    private DatePicker visitDateDatePicker;
+    @javafx.fxml.FXML
+    private TextField visitTimeTextField;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void saveVisitScheduleOnAction(ActionEvent actionEvent) {
+    }
+}
