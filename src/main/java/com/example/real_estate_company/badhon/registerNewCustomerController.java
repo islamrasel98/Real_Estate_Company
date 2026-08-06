74a3cb3652becfe78d0c75e624b0f6f81342a2bb
Badhon <nandibadhon0@gmail.com>
2026-08-06 22:34:47 +0600

    Register new customer fxml done

--- src/main/java/com/example/real_estate_company/badhon/registerNewCustomerController.java
+++ src/main/java/com/example/real_estate_company/badhon/registerNewCustomerController.java
@@ -1,7 +1,27 @@
 package com.example.real_estate_company.badhon;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class registerNewCustomerController
 {
     @javafx.fxml.FXML
+    private TextField phoneNumberTextField;
+    @javafx.fxml.FXML
+    private ComboBox propertyTypeComboBox;
+    @javafx.fxml.FXML
+    private TextField budgetTextField;
+    @javafx.fxml.FXML
+    private TextField emailTextField;
+    @javafx.fxml.FXML
+    private TextField nameTextField;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void saveInquiryOnAction(ActionEvent actionEvent) {
+    }
+}
