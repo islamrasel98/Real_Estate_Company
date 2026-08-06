0845c5ecc234d44272aea399e89f8e400d0ee97c
Badhon <nandibadhon0@gmail.com>
2026-08-06 23:00:22 +0600

    update sale lead fxml done

--- src/main/java/com/example/real_estate_company/badhon/updateSalesLeadStatusController.java
+++ src/main/java/com/example/real_estate_company/badhon/updateSalesLeadStatusController.java
@@ -1,7 +1,31 @@
 package com.example.real_estate_company.badhon;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class updateSalesLeadStatusController
 {
     @javafx.fxml.FXML
+    private TableColumn statusTableColumn;
+    @javafx.fxml.FXML
+    private Label LeadDetailsLabel;
+    @javafx.fxml.FXML
+    private ComboBox statusComboBox;
+    @javafx.fxml.FXML
+    private TableColumn customerTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn leadIdTableColumn;
+    @javafx.fxml.FXML
+    private TableView leadsTableView;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void saveStatusOnAction(ActionEvent actionEvent) {
+    }
+}
