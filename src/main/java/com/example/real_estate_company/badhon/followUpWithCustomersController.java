b755ed7285c2ae5082858ead454af7732171b58d
Badhon <nandibadhon0@gmail.com>
2026-08-06 21:36:33 +0600

    Follow up with coustomer fxml colmpete

--- src/main/java/com/example/real_estate_company/badhon/followUpWithCustomersController.java
+++ src/main/java/com/example/real_estate_company/badhon/followUpWithCustomersController.java
@@ -1,7 +1,18 @@
 package com.example.real_estate_company.badhon;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+
 public class followUpWithCustomersController
 {
     @javafx.fxml.FXML
+    private DatePicker customerFollowUpDatePicker;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void saveFollowUpOnAction(ActionEvent actionEvent) {
+    }
+}
