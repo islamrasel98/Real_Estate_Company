c846bb8236128a65ed625684046e06f35062cc5f
arafattanvir <2411710@iub.edu.bd>
2026-08-09 00:48:40 +0600

    E

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ module com.example.real_estate_company {
     requires javafx.fxml;
     requires java.desktop;
     requires javafx.graphics;
-    requires com.example.real_estate_company;
+
 
 
     opens com.example.real_estate_company to javafx.fxml;
